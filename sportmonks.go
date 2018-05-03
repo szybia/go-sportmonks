f56ccf2512125e1e011a1dec4324fe13266c8bea
BialkowskiSz <bialkowski.sz@gmail.com>
2018-05-03 22:05:35 +0100

    Unmarshalling JSON

--- sportmonks.go
+++ sportmonks.go
@@ -1,33 +1,52 @@
 package sportmonks
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
-	"os"
 )
 
 var apiToken = ""
 var apiURL = "https://soccer.sportmonks.com/api/v2.0/"
 
-//Test http request
-func Test() {
-	response, err := http.Get(apiURL)
-	var responseByte []byte
-	if err != nil {
-		os.Exit(1)
-	}
-	_, err = response.Body.Read(responseByte)
-	if err != nil {
-		os.Exit(0)
-	}
-}
-
-func setAPIToken(s string) {
+//SetAPIToken quite self-explanatory.
+func SetAPIToken(s string) {
 	if len(s) != 0 {
 		apiToken = s
 	}
 }
 
-func get(endpoint string) map[string]string {
+//Get API request.
+func Get(endpoint string) map[string]string {
 	payload := map[string]string{"api_token": apiToken}
+
+	req, err := http.NewRequest("GET", apiURL, nil)
+	if err != nil {
+		log.Print(err)
+	}
+
+	q := req.URL.Query()
+	q.Add("api_token", apiToken)
+	req.URL.RawQuery = q.Encode()
+
+	resp, err := http.Get(req.URL.String())
+	if err != nil {
+		log.Print(err)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Print(err)
+	}
+
+	var objmap map[string]interface{}
+	err = json.Unmarshal(body, &objmap)
+	if err != nil {
+		log.Print(err)
+	}
+
+	fmt.Println(req.URL.String())
 	return payload
 }
